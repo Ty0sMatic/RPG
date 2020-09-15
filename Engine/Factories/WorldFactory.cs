89ea7dfa1f63ebb2254b43bc4f5b98084abeba2a
Eric Herget <ericherget@gmx.de>
2020-09-15 13:27:22 +0200

    nix eigentlich

---
+++ Engine/Factories/WorldFactory.cs
@@ -0,0 +1,48 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Factories
+{
+    internal class WorldFactory
+    {
+        internal World CreateWorld()
+        {
+
+            World newWorld = new World();
+
+            newWorld.AddLocation(0, 0, "Home", "This is your Home",
+                "/Engine;component/Images/Locations/Home.png");
+
+            newWorld.AddLocation(-1, 0, "Herbalists Hut", "Herbs for medicine and potions are everywhere.",
+                "/Engine;component/Images/Locations/HerbalistsHut.png");
+
+            newWorld.AddLocation(-1, -1, "Herbalists Garden", "The Herbalists Garden. It smells of all different kinds of herbs.",
+                "/Engine;component/Images/Locations/HerbalistsGarden.png");
+
+            newWorld.AddLocation(0, -1, "Farmer's House", "The Farmer Edwin if your neighbor. This is where he lives.",
+                "/Engine;component/Images/Locations/Farmhouse.png");
+
+            newWorld.AddLocation(0, -2, "Farmers Field", "Wheat is neat. And it's everyhwere!",
+                "/Engine;component/Images/Locations/FarmFields.png");
+
+            newWorld.AddLocation(0, 1, "Town Square", "The town was built around a well. This is the center of the city.",
+                "/Engine;component/Images/Locations/TownSquare.png");
+
+            newWorld.AddLocation(1, 1, "Townsgate", "Behind it lies a spooky looking forest. A guard sleeps on a chair.",
+                "/Engine;component/Images/Locations/TownGate.png");
+
+            newWorld.AddLocation(2, 1, "Dark Forest", "You can barely see. You can also feel spider webs all over the place.",
+               "/Engine;component/Images/Locations/SpiderForest.png");
+
+            newWorld.AddLocation(1, 0, "Shop", "Here you could possibly buy potions, armor and weapons. But it's closed...",
+               "/Engine;component/Images/Locations/Trader.png");
+
+            return newWorld;
+        }
+
+    }
+}
---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+using Engine.Factories;
+
+namespace Engine.ViewModels
+{
+    public class GameSession : INotifyPropertyChanged
+    {
+        private Location _currentLocation;
+        public World CurrentWorld { get; set; }
+        public Player CurrentPlayer { get; set; }
+        public Location CurrentLocation
+        {
+            get { return _currentLocation; }
+            set
+            {
+                _currentLocation = value;
+
+                OnPropertyChanged("CurrentLocation");
+                OnPropertyChanged("HasLocationToNorth");
+                OnPropertyChanged("HasLocationToWest");
+                OnPropertyChanged("HasLocationToEast");
+                OnPropertyChanged("HasLocationToSouth");
+            }
+        }
+
+        public bool HasLocationtoNorth
+        {
+            get
+            {
+                return CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate + 1) != null;
+            }
+        }
+
+        public bool HasLocationtoSouth
+        {
+            get
+            {
+                return CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate - 1) != null;
+            }
+        }
+
+        public bool HasLocationtoEast
+        {
+            get
+            {
+                return CurrentWorld.LocationAt(CurrentLocation.XCoordinate + 1, CurrentLocation.YCoordinate) != null;
+            }
+        }
+
+        public bool HasLocationtoWest
+        {
+            get
+            {
+                return CurrentWorld.LocationAt(CurrentLocation.XCoordinate - 1, CurrentLocation.YCoordinate) != null;
+            }
+        }
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Hydra";
+            CurrentPlayer.CharacterClass = "Warrior";
+            CurrentPlayer.HitPoints = 10;
+            CurrentPlayer.Gold = 1000000;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
+
+            WorldFactory factory = new WorldFactory();
+            CurrentWorld = factory.CreateWorld();
+
+            CurrentLocation = CurrentWorld.LocationAt(0, 0);
+        }
+
+        public void MoveNorth()
+        {
+            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate + 1) ;
+        }
+
+        public void MoveEast()
+        {
+            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate + 1, CurrentLocation.YCoordinate);
+        }
+
+        public void MoveSouth()
+        {
+            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate - 1);
+        }
+
+        public void MoveWest()
+        {
+            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate - 1, CurrentLocation.YCoordinate);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
